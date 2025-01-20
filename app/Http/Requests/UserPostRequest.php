a24c17d88fb7a76f4406c04bec23cbb8b058cc28
Aditya Suryadi <adityasuryaditya@gmail.com>
2025-01-20 20:49:05 +0700

    fix:ignore unique validation on update

--- app/Http/Requests/UserPostRequest.php
+++ app/Http/Requests/UserPostRequest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Response as Resp;
+use Illuminate\Validation\Rule;
 
 class UserPostRequest extends FormRequest
 {
@@ -24,7 +25,6 @@ class UserPostRequest extends FormRequest
      */
     public function rules(): array
     {
-
         if ($this->isMethod('post')) {
             return [
                 'name' => 'required|string|max:255',
@@ -33,9 +33,18 @@ class UserPostRequest extends FormRequest
                 'password_confirmation' => 'required|string|min:8|same:password',
             ];
         } elseif ($this->isMethod('put')) {
+            if(!$this->user()){
+                throw new HttpResponseException(response()->notFound('user not found'));
+            }
             return [
                 'name' => 'required|string|max:255',
-                'email' => 'required|string|email|max:255|unique:users,email'. $this->user->id,
+                'email' => [
+                    'required',
+                    'string',
+                    'email',
+                    'max:255',
+                    Rule::unique('users','email')->ignore($this->user()->id),
+                ],
                 'password' => 'required|string|min:8',
                 'password_confirmation' => 'required|string|min:8|same:password',
             ];
